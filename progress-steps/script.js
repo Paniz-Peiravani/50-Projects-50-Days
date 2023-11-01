f09de28ce4b629813b788ee9596e79fca334ae95
Paniz Peiravani <paniz.peiravani@gmail.com>
2023-11-01 00:42:48 -0400

    Progress Steps

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -8,7 +8,7 @@ let currentActive = 1
 next.addEventListener('click', () => {
     currentActive++
 
-    if(currentActive > circles.length) {
+    if (currentActive > circles.length) {
         currentActive = circles.length
     }
 
@@ -18,7 +18,7 @@ next.addEventListener('click', () => {
 prev.addEventListener('click', () => {
     currentActive--
 
-    if(currentActive < 1) {
+    if (currentActive < 1) {
         currentActive = 1
     }
 
@@ -27,23 +27,31 @@ prev.addEventListener('click', () => {
 
 function update() {
     circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
+        if (idx < currentActive) {
+            circle.classList.add("active")
         } else {
-            circle.classList.remove('active')
+            circle.classList.remove("active")
         }
-    })
+
+    } )
 
     const actives = document.querySelectorAll('.active')
-
     progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-    if(currentActive === 1) {
+    if (currentActive === 1) {
         prev.disabled = true
-    } else if(currentActive === circles.length) {
+    } else if (currentActive === circles.length) {
         next.disabled = true
     } else {
-        prev.disabled = false
-        next.disabled = false
+        prev.disabled = false;
+        next.disable = false;
     }
+
+
+
 }
+
+
+
+
+
